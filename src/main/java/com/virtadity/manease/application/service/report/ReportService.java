34074d28bc373e1cc83aa063fc1fcb2ad49e4895
virtadity <virtadity@gmail.com>
2026-02-24 14:37:02 +0300

    Fix domain layer

--- src/main/java/com/virtadity/manease/application/service/report/ReportService.java
+++ src/main/java/com/virtadity/manease/application/service/report/ReportService.java
@@ -5,11 +5,9 @@ import com.virtadity.manease.application.model.report.ReportResponse;
 import com.virtadity.manease.application.port.in.report.ReportInputBoundary;
 import com.virtadity.manease.application.port.out.report_line.ReportLineOutputBoundary;
 import com.virtadity.manease.domain.model.Report;
-import com.virtadity.manease.domain.model.ReportLine;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 
@@ -22,18 +20,7 @@ public class ReportService implements ReportInputBoundary {
     @Override
     public ReportResponse execute(LocalDateTime fromDate, LocalDateTime toDate) {
         var reportLineList = reportLineStorage.getAllBetweenDates(fromDate, toDate);
-
-        var totalCost = reportLineList
-                .parallelStream()
-                .map(ReportLine::totalSupplyCost)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-        var totalWeight = reportLineList
-                .parallelStream()
-                .map(ReportLine::totalSupplyWeight)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-        var report = new Report(fromDate, toDate, totalCost, totalWeight, reportLineList);
+        var report = Report.fromRecordLineList(reportLineList, fromDate, toDate);
         return reportMapper.toReportResponse(report);
     }
 }
